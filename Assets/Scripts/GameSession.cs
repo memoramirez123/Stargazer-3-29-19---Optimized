31d4347e68be8ed66b6178e4d2b6183b6635d837
memoramirez123 <guillermoramirez123@yahoo.com>
2019-03-29 13:56:54 -0700

    Optimized Version of Game
    Optimized

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameSession : MonoBehaviour {
+
+    [SerializeField] int playerDeaths = 0;
+    [SerializeField] Text deathText;
+    [SerializeField] Text timerText;
+    [SerializeField] float startTime;
+
+    private void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        } 
+     }
+
+    // Use this for initialization
+    void Start () 
+    {
+        startTime = Time.time;
+		// deathText.text = "Deaths: " + playerDeaths.ToString();
+	}
+	
+    private void Update()
+    {
+        float t = Time.time-startTime;
+        string minutes = ((int)t / 60).ToString("00");
+        string seconds = (t % 60).ToString("00.00");
+        timerText.text = "Time\n " + minutes + ":" + seconds;
+         deathText.text = "Deaths: " + playerDeaths.ToString();
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        // if (playerLives > 1)
+        // {
+        //     incrementDeaths();
+        // }
+        // else
+        // {
+        //     ResetGameSession();
+        // }
+        if(playerDeaths >= 0){
+            incrementDeaths();
+        }
+    }
+
+    private void incrementDeaths()
+    {
+        playerDeaths++;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        deathText.text = "Deaths: " + playerDeaths.ToString();
+    }
+
+    private void ResetGameSession()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Level Transition Scripts/LevelFaderScript.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFaderScript : MonoBehaviour {
+
+	public Animator animator;
+
+	private int levelToLoad;
+	// Update is called once per frame
+	 void Update () {
+		 if(Input.GetMouseButtonDown(0))
+	 		FadeToLevel();
+	 }
+
+	public void FadeToLevel ()
+	{
+		levelToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+		animator.SetTrigger("FadeOut");
+	}
+
+	public void OnFadeComplete ()
+	{
+		SceneManager.LoadScene(levelToLoad);
+	}
+}
